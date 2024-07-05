6e82927489b314339af50a039fbe20cfb9ad3b68
sobhysalem <111722622+sobhysalem@users.noreply.github.com>
2024-07-06 00:27:25 +0300

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,9 +1,10 @@
+#define  ll  long long
 const int N = 1e5+5;  
 vector<pair<int, int>> adj[N]; 
 
 vector<long long> Dijkstra(int src, int n) {
-    vector<long long> dist(n + 5, -1);
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> q;
+    vector<ll> dist(n + 5, -1);
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> q;
     q.push({0, src});
     while (!q.empty()) {
         int u = q.top().second;
