d145ee8a505ece36f337bb734ca1b5bdfa8b463f
sobhysalem <111722622+sobhysalem@users.noreply.github.com>
2024-07-06 00:37:06 +0300

    Create floydWarshall.cpp

---
+++ floydWarshall.cpp
@@ -0,0 +1,13 @@
+const int INF = 1e9;
+const int N = 1000;  
+vector<vector<int>> dist(N, vector<int>(N, INF));
+void floydWarshall(int n) {
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
