81c7b506fe266404a7694bb1f956ab06c5a8717b
sobhysalem <111722622+sobhysalem@users.noreply.github.com>
2024-08-14 03:56:16 +0300

    Update nCr&NpR.cpp

--- nCr&NpR.cpp
+++ nCr&NpR.cpp
@@ -1,6 +1,6 @@
-const int MOD = 1e9 + 7;
- 
-ll modinv(ll a, ll m = MOD){ 
+vector < ll > fact(1e6 + 1, 1);
+vector < ll > inv(1e6 + 1, 1);
+ll modinv(ll a, ll m = MOD){
     ll y = 0, x = 1;
     if (m == 1) return 0;
     while (a > 1){
@@ -17,19 +17,13 @@ ll modinv(ll a, ll m = MOD){
 void factorial(){
     for(int i = 2; i <= 1e6; i++)
         fact[i] = (fact[i - 1] * i) % MOD;
+    inv[1e6]=modinv(fact[1e6]);
+    for(int i=1e6-1;i>=0;i--){
+        (inv[i]=inv[i+1]*(i+1));
+    }
 }
- 
+
 ll nCr(ll n, ll r){
     if(r > n) return 0;
-    return (fact[n] * modinv(fact[r]) % MOD * modinv(fact[n - r]) % MOD) % MOD;
-}
- 
-// NPR =fact(n)/fact(n-r);
-ll NpR(ll n, ll r){
-	ll ans = 1;
-	for (ll i = (n - r) + 1; i <= n; i++){
-		ans *= i;
-		ans %= MOD;
-	}
-	return ans;
+    return (fact[n] * inv[r] % MOD * inv[n - r] % MOD) % MOD;
 }
