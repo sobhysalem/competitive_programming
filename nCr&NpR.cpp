7d8d1f2bf85640992ac2c0d3bc992e04d42c872e
sobhysalem <111722622+sobhysalem@users.noreply.github.com>
2024-08-14 03:31:37 +0300

    Update nCr&NpR.cpp

--- nCr&NpR.cpp
+++ nCr&NpR.cpp
@@ -1,4 +1,19 @@
-
+const int MOD = 1e9 + 7;
+ 
+ll modinv(ll a, ll m = MOD){ 
+    ll y = 0, x = 1;
+    if (m == 1) return 0;
+    while (a > 1){
+        ll q = a / m;
+        ll t = m;
+        m = a % m, a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if (x < 0) x += MOD;
+    return x;
+}
 void factorial(){
     for(int i = 2; i <= 1e6; i++)
         fact[i] = (fact[i - 1] * i) % MOD;
